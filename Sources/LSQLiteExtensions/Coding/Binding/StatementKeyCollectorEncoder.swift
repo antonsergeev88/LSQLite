d568803960de6842e6f958d06fd52f556ce30f67
Anton Sergeev <antonsergeev88@gmail.com>
2026-02-02 16:27:35 +0200

    Remove duplicated specialized methods

--- Sources/LSQLiteExtensions/Coding/Binding/StatementKeyCollectorEncoder.swift
+++ Sources/LSQLiteExtensions/Coding/Binding/StatementKeyCollectorEncoder.swift
@@ -45,62 +45,6 @@ struct StatementKeyCollectorContainer<Key: CodingKey>: KeyedEncodingContainerPro
         encoder.record(key)
     }
 
-    mutating func encode(_ value: Bool, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: String, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Double, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Float, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Int, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Int8, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Int16, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Int32, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: Int64, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: UInt, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: UInt8, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: UInt16, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: UInt32, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
-    mutating func encode(_ value: UInt64, forKey key: Key) throws {
-        encoder.record(key)
-    }
-
     mutating func encode<T: Encodable>(_ value: T, forKey key: Key) throws {
         encoder.record(key)
     }
